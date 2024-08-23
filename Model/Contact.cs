e2769a1cc91f7fed0a3edf02eb8127bebc733b06
moha_amini <mhaminii18@gmail.com>
2024-08-23 17:51:02 +0330

    add contact model

---
+++ Model/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
